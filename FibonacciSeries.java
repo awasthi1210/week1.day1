a911b9dac31b7211af516ebe56ade8e8300ec8a5
awasthi1210 <107309239+awasthi1210@users.noreply.github.com>
2022-06-11 21:18:57 +0530

    Update FibonacciSeries.java

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -6,16 +6,16 @@ public class FibonacciSeries {
 		// TODO Auto-generated method stub
 		int firstNum = 0;
 		int secondNum = 1;
+		int range =7;
 		int sum = 0;
 		System.out.println(firstNum);
 		System.out.println(secondNum);
-		for (int i = 1; i<6; i++) {
-			
+		for (int i = 1; i < range; i++) {
+
 			sum = firstNum + secondNum;
 			firstNum = secondNum;
 			secondNum = sum;
 			System.out.println(sum);
-			
 
 		}
 	}
