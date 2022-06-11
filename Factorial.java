434f7a775af91bd233810ac794384de5a457344d
awasthi1210 <107309239+awasthi1210@users.noreply.github.com>
2022-06-11 21:12:54 +0530

    removing white spaces

--- Factorial.java
+++ Factorial.java
@@ -4,12 +4,10 @@ public class Factorial {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
 		int input=5;
 		int fact=1;
 		for(int i=5;i>=1;i--) {
 			fact = fact*i;	
-			
 		}
 		System.out.println(fact);
 	}
